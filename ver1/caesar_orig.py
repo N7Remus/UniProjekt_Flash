575ae4931042581e0f0867ce2ecdf52c78bbf6e7
N7Remus <remaigabor@gmail.com>
2018-11-09 21:15:44 +0100

    Python Caesar code ver 1

---
+++ ver1/caesar_orig.py
@@ -0,0 +1,16 @@
+import string
+
+#a = input("input : ")
+a= "Ha keresunk valamit, sose talaljuk meg. Akkor talaljuk meg, amikor nem keressuk. Gayle Forman"
+#I9	jfqfrvml~w9m9nhu+	rprf~u9m9mivj	lff/~Bjlns~u9m9mivj	lff-~bljjpq	mfl	jfqfrttl-	Fbxmd	Epqn9o
+
+db = string.printable
+b=""
+a_chars = list(a)
+#print(db)
+c=1
+for i in range(0, len(a_chars)):
+	db_index=db.index(a_chars[i])
+	b+=db[db_index+(c)]
+	c=c*-1
+print(b)
