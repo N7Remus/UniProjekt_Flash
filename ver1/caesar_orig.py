66c25c9080d44398c44bb54a2e9507538d09b1f3
N7Remus <remaigabor@gmail.com>
2018-11-09 21:44:25 +0100

    Add files via upload

--- ver1/caesar_orig.py
+++ ver1/caesar_orig.py
@@ -2,14 +2,20 @@ import string
 
 #a = input("input : ")
 a= "Ha keresunk valamit, sose talaljuk meg. Akkor talaljuk meg, amikor nem keressuk. Gayle Forman"
-
+#a="IZ jfqfrvml wZmZnhu, rprf uZmZmivj lff. Bjlns uZmZmivj lff, bljjpq mfl jfqfrttl. Fbxmd EpqnZo"
 db = string.ascii_letters
 b=""
 a_chars = list(a)
 #print(db)
-c=1
+c=-1
 for i in range(0, len(a_chars)):
-	db_index=db.index(a_chars[i])
-	b+=db[db_index+(c)]
+	if a_chars[i] in db:
+		db_index=db.index(a_chars[i])
+		if db_index+c==len(db):
+			b += db[0]
+		else:
+			b+=db[db_index+(c)]
+	else:
+		b +=a_chars[i]
 	c=c*-1
 print(b)
